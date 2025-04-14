d976d31aa799acbbe6173500412799a5c07975c5
ostrich-egg <sauhardhakafle12@gmail.com>
2025-04-14 16:56:30 +0545

    Workable frontend

--- app.py
+++ app.py
@@ -19,12 +19,18 @@ def home():
 @app.route("/predict_api", methods=["POST"])
 def predict_api():
     data = request.json["data"]
-    print("got data: ", data)
     fmt_data = np.array(list(data.values())).reshape(1, -1)
-    new_data = scaler.transform(fmt_data)
-    output = forestmodel.predict(new_data)
-    print("predicted output: ", output)
+    final_input = scaler.transform(fmt_data)
+    output = forestmodel.predict(final_input)
     return jsonify(output[0])
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    output = forestmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text=output)
 
 
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
 ## House Price prediction
 
--> This code is based on the RandomForestRegression model.
--> Dataset is of the california housing, available on `sklearn.datasets`
+> First run the code present on `*.ipynb` file, so it will generate the `.pkl` file, which is require to run this code.
+
+
+## Info
+
+- This code is based on the RandomForestRegression model.
+- Dataset is of the california housing, available on `sklearn.datasets`
